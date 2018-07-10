d2ce1a9d5eae8598e4a370f163f994b9c6470deb
Yongsheng <zhaoyongsheng@zju.edu.cn>
2018-07-10 21:57:04 +0800

    add vnc server configuration files.

---
+++ vncserver.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+#======================================================================
+#   Copyright (C) 2018 Institute of Cyber-Systems & Control.
+#   
+#           File Name: vncserver.sh
+#              Author: Dr. Yongsheng Zhao
+#               Email: zhaoyongsheng@zju.edu.cn
+#       Creating Data: 07 10, 2018
+#        Discription: 
+#
+#======================================================================
+
+vncserver :1 -geometry 1920x1080 > ~/Downloads/vncserver.log 2>&1
